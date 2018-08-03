8679e43d79a48da0ba253a7e70e901354aca9c12
Stefano Vazzoler <stefanovazzocell@gmail.com>
2018-08-04 00:18:08 +0200

    Last commit

--- src/public_cdn/js/ui.js
+++ src/public_cdn/js/ui.js
@@ -12,9 +12,30 @@ function msg(message) {
 	}, 5000);
 }
 
+function updateHours() {
+	var hours = parseInt($$('#hours').val());
+	var days = Math.floor(hours / 24);
+	$$('#aDays').html(days);
+	$$('#aHours').html(hours - 24 * days);
+}
+
 $$().ready(function() {
 	$$('#alert').onClick(function() {
 		$$('#alert').addClass('hidden');
 		clearTimeout(messageTimer);
+	});
+	$$('input').event('focus', function() {
+		$$('#footer').addClass('hidden');
+		$$('#content').style('bottom', '0');
+	});
+	$$('input').event('blur', function() {
+		$$('#footer').removeClass('hidden');
+		$$('#content').style('bottom', '30px');
+	});
+	$$('#hours').event('input', function() {
+		updateHours();
+	});
+	$$('.hours').event('click', function() {
+		updateHours();
 	});
 });
