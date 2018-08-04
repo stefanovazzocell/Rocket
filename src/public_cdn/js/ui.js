a9a6bba544547e701cdc794bb41b36362d3d2136
Stefano Vazzoler <stefanovazzocell@gmail.com>
2018-08-04 11:04:05 +0200

    More/Less Options

--- src/public_cdn/js/ui.js
+++ src/public_cdn/js/ui.js
@@ -1,3 +1,16 @@
 'use strict';
 
-$$().ready(function() {});
+function autoCollapse(id) {
+	var btn = $$('#' + id + 'Btn');
+	btn.onClick(function() {
+		$$('#' + id).toggleClass('show');
+		btn.toggleClass('collapsed');
+		if (btn.html() == 'Less options') {
+			btn.html('More options');
+		} else btn.html('Less options');
+	});
+}
+
+$$().ready(function() {
+	autoCollapse('opt');
+});
