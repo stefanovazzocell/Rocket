3e878a5d65a6d74f58c3fd616a0f8446467e829a
Stefano Vazzoler <stefanovazzocell@gmail.com>
2018-08-04 12:04:36 +0200

    Alert helpers

--- src/public_cdn/js/ui.js
+++ src/public_cdn/js/ui.js
@@ -1,16 +1,52 @@
 'use strict';
 
+// Track for alertTimeout
+var alertTimeout;
+
 /*
 * autoCollapse(id) - Set an event to collapse cards
 *
 * @requires id string to be id of element
 */
 function autoCollapse(id) {
+	// Get btn
 	var btn = $$('#' + id + 'Btn');
+	// Set event
 	btn.onClick(function() {
+		// Toggle classes
 		$$('#' + id).toggleClass('show');
 		btn.toggleClass('collapsed');
 	});
+}
+
+/*
+* hideAlert() - Hides an alert and stops the timer
+*/
+function hideAlert() {
+	// Remove previous timer
+	clearTimeout(alertTimeout);
+	// Hide the alert
+	$$('#alert').addClass('hidden');
+	$$('#alert').removeClass('show');
+}
+
+/*
+* msg(message) - Show an alert
+*
+* @requires message string to be message to show
+*/
+function msg(message) {
+	// Log to console
+	console.log(message);
+	// Reset alert
+	hideAlert();
+	// Set alert message
+	$$('#alert').html(message);
+	// Show alert
+	$$('#alert').addClass('show');
+	$$('#alert').removeClass('hidden');
+	// Hiden the alert after 5 seconds
+	alertTimeout = setTimeout(hideAlert, 5000);
 }
 
 /*
@@ -20,7 +56,9 @@ function autoCollapse(id) {
 * @requires obj array of a 2 elents array [[, ],[, ]] or strings [,]
 */
 function applyToAll(fn, obj) {
+	// For each element in the array
 	for (var i = obj.length - 1; i >= 0; i--) {
+		// Call function
 		if (typeof obj[i] === 'string') {
 			fn(obj[i]);
 		} else {
@@ -32,4 +70,6 @@ function applyToAll(fn, obj) {
 $$().ready(function() {
 	// Auto Collapse options
 	autoCollapse('opt');
+	// Hide the alert if clicked
+	$$('#alert').onClick(hideAlert);
 });
