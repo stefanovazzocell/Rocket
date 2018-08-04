e7ada9eb0f5ba512be0b3454e2c93b1813032944
Stefano Vazzoler <stefanovazzocell@gmail.com>
2018-08-04 22:43:23 +0200

    added basic encrypt and hashing function

--- src/public_cdn/js/encryption.js
+++ src/public_cdn/js/encryption.js
@@ -1,48 +1,38 @@
-const salt = "jj";
+const salt = 't6IcYm1fvwo0O+NwEo9JAqKwcn88zeqO/U1DZvCsgzK3GGa1QzOZpRNs2/sr17d7HNsIsoXhaTbjMk7sMRr5Rw==';
 
 /*
 * encrypt(data, password) - AES Encryption
 *
-* @requires data string is data to be encrypted
-* @requires password string is data to be encrypted 
+* @requires data String is data to be encrypted
+* @requires password String is the password to use
 */
 function encrypt(data, password) {
-	return sjcl.encrypt(password, data, {mode : "gcm"});
+	return sjcl.encrypt(password, data, {mode : 'gcm'});
 }
 
 /*
-* pbkdf2(data) - PBKDF2 with the Rocket salt, 100000 rounds
+* hash(data) - PBKDF2 with the Rocket salt + sha512
 *
-* @requires data string is data to be hashed 
+* @requires data String is data to be hashed 
 */
-function pbkdf2(data) {
-	return sjcl.misc.pbkdf2(password, salt, 100000);
+function hash(data, iter = 100000) {
+	return sjcl.codec.base64.fromBits(sjcl.hash.sha512.hash(sjcl.misc.pbkdf2(data, salt, iter)));
 }
 
 /*
-* pbkdf2(data) - PBKDF2 Hashing, 100000 rounds
+* encryptData(data, parameters, link, onSuccess, onFailure, addPassword) - Encryption Utility for Rocket
 *
-* @requires data string is data to be hashed 
+* @requires data String is primary data to be encrypted
+* @requires parameters Object is secondary data to be encrypted
+* @requires link String is short link to use
+* @requires addPassword String is additional password or false
+* @requires onSuccess Function to call if Success
+* @requires onFailure Function to call if Failure
+* @returns Object containing 'l', 'd', 'p' parameters
 */
-function pbkdf2(data) {
-	return sjcl.misc.pbkdf2(password, salt, 100000);
-}
-
-/*
-* sha512(data) - sha256 Hashing
-*
-* @requires data string is data to be hashed 
-*/
-function sha512(data) {
-	return sjcl.hash.sha512(data);
-}
-
-/*
-* encryptData(data) - Encryption Utility for Rocket
-*
-* @requires data string is data to be encrypted
-* @requires password string is data to be encrypted 
-*/
-function encryptData(data, s, hash) {
+function encryptData(data, parameters, link, onSuccess, onFailure = msg, addPassword=false) {
+	if (addPassword !== false) {
+		
+	}
 	//var data ;
 }
