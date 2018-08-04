eb6c3bfa816136ef7a50326e40907016b6d12c2f
Stefano Vazzoler <stefanovazzocell@gmail.com>
2018-08-04 11:37:00 +0200

    improvevemnts

--- src/public_cdn/js/ui.js
+++ src/public_cdn/js/ui.js
@@ -10,9 +10,6 @@ function autoCollapse(id) {
 	btn.onClick(function() {
 		$$('#' + id).toggleClass('show');
 		btn.toggleClass('collapsed');
-		if (btn.html() == 'Less options') {
-			btn.html('More options');
-		} else btn.html('Less options');
 	});
 }
 
@@ -20,15 +17,19 @@ function autoCollapse(id) {
 * applyToAll(fn, obj) - Apply function to all objects
 *
 * @requires fn function to apply
-* @requires obj array of a 2 elents array [[id, param],[id, param]]
+* @requires obj array of a 2 elents array [[, ],[, ]] or strings [,]
 */
 function applyToAll(fn, obj) {
 	for (var i = obj.length - 1; i >= 0; i--) {
-		fn(obj[i][0], obj[i][1]);
+		if (typeof obj[i] === 'string') {
+			fn(obj[i]);
+		} else {
+			fn(obj[i][0], obj[i][1]);
+		}
 	}
 }
 
 $$().ready(function() {
-	// Auto Collapse
+	// Auto Collapse options
 	autoCollapse('opt');
 });
