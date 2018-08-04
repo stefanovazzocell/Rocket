80f557c248f1d7573be53701be589495eb07f308
Stefano Vazzoler <stefanovazzocell@gmail.com>
2018-08-04 16:29:44 +0200

    Added function to show stats

--- src/public_cdn/js/_manage.js
+++ src/public_cdn/js/_manage.js
@@ -2,6 +2,44 @@
 
 var selected = 'stats';
 
+/*
+* showStats(values) - Shows the stats for each country
+*
+* @requires values object { "??": 1 }
+*/
+function showStats(values = {}) {
+	var countries = Object.keys(values);
+	var sum = 0;
+	var htmlList = '';
+	// Get the html ready
+	for (var i = countries.length - 1; i >= 0; i--) {
+		sum += values[countries[i]];
+		htmlList += '<li class="list-group-item d-flex justify-content-between align-items-center">';
+		if (countries[i] == '??') {
+			htmlList += 'N/A';
+		} else if (countries[i] == 'XX') {
+			htmlList += 'Unknown';
+		} else {
+			htmlList += countries[i];
+		}
+		htmlList += '<span class="badge badge-primary badge-pill">' + values[countries[i]] + '</span></li>';
+	}
+	// Load the html
+	$$('#countriesList').html(htmlList);
+	// Update the map
+	try {
+		var map = $$('#worldMap').first().getSVGDocument();
+		for (var i = countries.length - 1; i >= 0; i--) {
+			if (countries[i] != '??' && countries[i] != 'XX') {
+				var strength = (values[countries[i]]/sum) * 255;
+				map.querySelector('[cc=' + countries[i].toLowerCase() + ']').style.fill = 'rgb(' + (255 - strength) + ',' + strength + ',255)';
+			}
+		}
+	} catch (err) {
+		msg('Couldn\'t paint the map');
+	}
+}
+
 // When page ready start functions
 $$().ready(function() {
 	// Enable checkboxes
