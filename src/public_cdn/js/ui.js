f911cdf20e815f6d93f91d738545a02dc7213c84
Stefano Vazzoler <stefanovazzocell@gmail.com>
2018-08-04 11:09:31 +0200

    Implemented special map function

--- src/public_cdn/js/ui.js
+++ src/public_cdn/js/ui.js
@@ -1,5 +1,10 @@
 'use strict';
 
+/*
+* autoCollapse(id) - Set an event to collapse cards
+*
+* @requires id string to be id of element
+*/
 function autoCollapse(id) {
 	var btn = $$('#' + id + 'Btn');
 	btn.onClick(function() {
@@ -11,6 +16,19 @@ function autoCollapse(id) {
 	});
 }
 
+/*
+* applyToAll(fn, obj) - Apply function to all objects
+*
+* @requires fn function to apply
+* @requires obj array of a 2 elents array [[id, param],[id, param]]
+*/
+function applyToAll(fn, obj) {
+	for (var i = obj.length - 1; i >= 0; i--) {
+		fn(obj[i][0], obj[i][1]);
+	}
+}
+
 $$().ready(function() {
+	// Auto Collapse
 	autoCollapse('opt');
 });
